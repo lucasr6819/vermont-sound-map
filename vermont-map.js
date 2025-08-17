7f959a334ea81ee7edb995430781532caee783b9
Lucas Richardson <lucas6819@Lucass-MBP.lan>
2025-08-17 13:53:25 -0400

    Update source rendering and source data for heatmap popups; all special locations now have correct links and robust rendering

--- vermont-map.js
+++ vermont-map.js
@@ -418,26 +418,189 @@ function createHeatmap(query = '') {
 
             // Fix: Render sources correctly for both string and object formats
             let sourcesHtml = '';
-            if (item.sources && Array.isArray(item.sources)) {
-                // Filter out undefined, null, or empty strings
-                const validSources = item.sources.filter(source => source && source !== '' && source !== undefined);
+            // Special case: If the item matches one of the known keys, use the provided links
+            const specialSources = {
+                "f35_fighter_jets_burlington_airport": [
+                    "https://www.communitynews.net/home/on-the-f-35s-noise-pollution-as-an-environmental-injustice",
+                    "https://vtdigger.org/2018/02/27/already-battered-thunderous-noise-community-braces-arrival-f-35/",
+                    "https://www.sevendaysvt.com/OffMessage/archives/2019/05/28/many-more-homes-are-inside-high-decibel-areas-on-new-f-35-sound-maps",
+                    "https://www.btvsound.com/public-portal/",
+                    "https://www.vtcng.com/otherpapersbvt/news/around_town/updated-noise-exposure-map-for-f-35s-released-fewer-south-burlington-homes-in-65-decibel/article_7a70c07d-daf0-5cd1-825b-dfac71ef266d.html",
+                    "https://www.nature.com/articles/s41370-024-00725-3",
+                    "https://vtdigger.org/2024/10/13/south-burlington-city-council-passes-f-35-resolution/",
+                    "https://www.vtcng.com/otherpapersbvt/news/municipal_matters/south-burlington-city-council-passes-f-35-resolution/article_2cf79ea6-871e-11ef-90c1-8f9ee29a4c5a.html",
+                    "https://saveourskiesvt.org/evidence-of-corruption-of-the-f-35-burlington-basing-process/",
+                    "https://www.vermontpublic.org/vpr-news/2013-04-15/leahy-responds-to-f-35-preliminary-approval",
+                    "https://vtdigger.org/2018/03/05/leahy-staff-central-role-f-35-basing-decision/",
+                    "https://www.vermontpublic.org/podcast/brave-little-state/2021-07-22/why-is-the-f-35-based-in-vermont",
+                    "https://www.airforcetimes.com/news/your-air-force/2019/10/01/air-force-rejects-request-for-further-f-35-noise-study/"
+                ],
+                "rock_of_ages_granite_quarry": [
+                    "https://vtdigger.org/2018/06/22/barre-rock-crusher-granted-permit-despite-neighbor-opposition/",
+                    "https://law.justia.com/cases/vermont/supreme-court/2019/2018-249.html",
+                    "https://protectgranitevillevt.wordpress.com/2017/07/06/neighbors-fault-state-for-lax-quarry-regulation/",
+                    "https://protectgranitevillevt.wordpress.com/current-news/",
+                    "https://vtdigger.org/2019/07/16/after-a-decade-of-gripes-in-graniteville-neighbors-question-state-response/",
+                    "https://vtdigger.org/2019/08/26/supreme-court-sides-with-owners-of-graniteville-rock-crushing-operation/",
+                    "https://www.vermontlaw.edu/academics/clinics-and-externships/ENRLC/cases/opposing-an-asphalt-plant-in-local-residential-community",
+                    "https://www.enoisecontrol.com/summary-msha-noise-guidelines-safety-regulations/",
+                    "https://dec.vermont.gov/air-quality/permits/source-categories/quarries-and-rock-crushing"
+                ],
+                "sheffield_wind_farm": [
+                    "https://www.vermontpublic.org/vpr-news/2015-04-01/fish-and-wildlife-studies-impact-of-searsburg-wind-development-on-black-bears",
+                    "http://vtfishandwildlife.com/conserve/deerfield-wind-bear-study"
+                ],
+                "searsburg_wind_farm": [
+                    "https://www.vermontpublic.org/vpr-news/2015-04-01/fish-and-wildlife-studies-impact-of-searsburg-wind-development-on-black-bears",
+                    "http://vtfishandwildlife.com/conserve/deerfield-wind-bear-study"
+                ],
+                "kingdom_community_wind_farm": [
+                    "https://www.vermontpublic.org/vpr-news/2015-04-01/fish-and-wildlife-studies-impact-of-searsburg-wind-development-on-black-bears",
+                    "http://vtfishandwildlife.com/conserve/deerfield-wind-bear-study"
+                ],
+                "interstate_89_corridor": [
+                    "https://www.uvm.edu/cems/trc/current-projects",
+                    "https://vtrans.vermont.gov/",
+                    "https://vtrans.vermont.gov/projects/colchester-exit17/project-updates",
+                    "https://vtrans.vermont.gov/road-construction"
+                ],
+                "amtrak_vermonter_service": [
+                    "https://www.uvm.edu/cems/trc/current-projects",
+                    "https://vtrans.vermont.gov/"
+                ],
+                "vermont_route_100": [
+                    "https://www.uvm.edu/cems/trc/current-projects",
+                    "https://vtrans.vermont.gov/"
+                ],
+                "ben_jerrys_factory": [],
+                "uvm_medical_center": [
+                    "https://www.weho.org/city-government/city-departments/community-safety/public-safety/helicopter-noise"
+                ],
+                "globalfoundries_essex": [
+                    "https://vtdigger.org/2021/04/28/globalfoundries-wants-its-essex-junction-plant-to-produce-its-own-energy/"
+                ],
+                "mcneil_generating_station": [
+                    "https://www.stopbtvbiomass.org/mcneil",
+                    "https://www.burlingtonvt.gov/539/Noise-Pollution"
+                ],
+                "killington_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming",
+                    "https://www.glassdoor.com/Reviews/Employee-Review-Killington-Ski-Resort-RVW14734107.htm"
+                ],
+                "stowe_mountain_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "jay_peak_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "vermont_rail_system": [
+                    "https://www.uvm.edu/cems/trc/current-projects",
+                    "https://vtrans.vermont.gov/"
+                ],
+                "cabot_creamery": [],
+                "lake_champlain_ferries": [
+                    "https://www.uvm.edu/cems/trc/current-projects",
+                    "https://vtrans.vermont.gov/"
+                ],
+                "patrick_leahy_airport_general": [
+                    "https://www.communitynews.net/home/on-the-f-35s-noise-pollution-as-an-environmental-injustice",
+                    "https://www.btvsound.com/public-portal/",
+                    "https://www.sevendaysvt.com/OffMessage/archives/2019/05/28/many-more-homes-are-inside-high-decibel-areas-on-new-f-35-sound-maps"
+                ],
+                "state_highway_construction": [
+                    "https://vtrans.vermont.gov/road-construction",
+                    "https://vtrans.vermont.gov/projects/colchester-exit17/project-updates",
+                    "https://www.stateregstoday.com/living/environmental-protection/noise-pollution-control-in-vermont",
+                    "https://environment.transportation.org/focus-areas/noise/noise-overview/"
+                ],
+                "velco_infrastructure": [
+                    "https://www.velco.com/projects/highgate-substation",
+                    "https://www.velco.com/what-we-do"
+                ],
+                "vermont_trucking_freight": [
+                    "https://www.stateregstoday.com/living/environmental-protection/noise-pollution-control-in-vermont",
+                    "https://environment.transportation.org/focus-areas/noise/noise-overview/"
+                ],
+                "vermont_quarrying_slate_valley": [
+                    "https://vtdigger.org/2019/08/26/supreme-court-sides-with-owners-of-graniteville-rock-crushing-operation/",
+                    "https://www.vermontlaw.edu/academics/clinics-and-externships/ENRLC/cases/opposing-an-asphalt-plant-in-local-residential-community",
+                    "https://protectgranitevillevt.wordpress.com/current-news/",
+                    "https://dec.vermont.gov/air-quality/permits/source-categories/quarries-and-rock-crushing"
+                ],
+                "vermont_emergency_medical_services": [
+                    "https://www.weho.org/city-government/city-departments/community-safety/public-safety/helicopter-noise"
+                ],
+                "burlington_fire_department": [
+                    "https://www.burlingtonvt.gov/539/Noise-Pollution"
+                ],
+                "montpelier_fire_ambulance": [
+                    "https://www.burlingtonvt.gov/539/Noise-Pollution"
+                ],
+                "vermont_state_emergency_operations": [
+                    "https://www.weho.org/city-government/city-departments/community-safety/public-safety/helicopter-noise"
+                ],
+                "bolton_valley_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "smugglers_notch_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "mad_river_glen": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "middlebury_college_snow_bowl": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "stratton_mountain_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "burke_mountain_ski_academy": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "ascutney_mountain_resort": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ],
+                "cochrans_ski_area": [
+                    "https://www.change.org/p/stowe-select-board-support-a-noise-ordinance-for-stowe",
+                    "https://www.vermontpublic.org/local-news/2024-02-27/greener-snowmaking-is-helping-ski-resorts-weather-climate-change",
+                    "https://skivermont.com/snowmaking-grooming"
+                ]
+            };
+            let sourcesToRender = item.sources;
+            if (specialSources[item.id]) {
+                sourcesToRender = specialSources[item.id];
+            }
+            if (sourcesToRender && Array.isArray(sourcesToRender)) {
+                const validSources = sourcesToRender.filter(source => typeof source === 'string' && source.trim() !== '');
                 if (validSources.length === 0) {
                     sourcesHtml = '<li>No sources available</li>';
                 } else {
                     sourcesHtml = validSources.map(source => {
-                        if (typeof source === 'string') {
-                            // If string looks like a URL, render as link
-                            if (/^(https?:\/\/)/.test(source)) {
-                                return `<li><a href="${source}" target="_blank">${source}</a></li>`;
-                            } else {
-                                return `<li>${source}</li>`;
-                            }
-                        } else if (typeof source === 'object' && source.text && source.url) {
-                            return `<li><a href="${source.url}" target="_blank">${source.text}</a></li>`;
-                        } else if (typeof source === 'object' && source.text) {
-                            return `<li>${source.text}</li>`;
+                        if (/^(https?:\/\/)/.test(source)) {
+                            return `<li><a href="${source}" target="_blank">${source}</a></li>`;
+                        } else {
+                            return `<li>${source}</li>`;
                         }
-                        return '';
                     }).join('');
                 }
             } else {
